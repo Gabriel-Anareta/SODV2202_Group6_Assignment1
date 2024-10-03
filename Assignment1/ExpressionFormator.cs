967c90e86e446f1cd635e6d9e133e16c5f1ec2b9
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-03 09:04:40 -0600

    Implemented checking for negative values

--- Assignment1/ExpressionFormator.cs
+++ Assignment1/ExpressionFormator.cs
@@ -8,9 +8,25 @@ namespace Assignment1
 {
     internal class ExpressionFormator
     {
-        public string InfixToPostfix()
+        public string InfixToPostfix(string infix)
         {
+            StringBuilder postfix = new StringBuilder();
+            Stack<char> stack = new Stack<char>();
+            List<char> operators = new List<char> { '*', '/', '+', '-' };
 
+            for (int i = 0; i < infix.Length; i++)
+            {
+                bool isPositive = true;
+                if (                                // checking for negatives
+                    infix[i] == '-'
+                    && Char.IsDigit(infix[i + 1])
+                )
+                {
+                    isPositive = false;
+                }
+            }
+
+            return "";
         }
     }
 }
