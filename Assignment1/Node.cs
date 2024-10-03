1d15a354da54d068c08defc60dd3a9daf31d9abc
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-03 09:59:31 -0600

    Implemented Node constructors

--- Assignment1/Node.cs
+++ Assignment1/Node.cs
@@ -8,5 +8,22 @@ namespace Assignment1
 {
     internal class Node
     {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public string Value { get; set; }
+
+        public Node(char value)
+        {
+            Left = null;
+            Right = null;
+            Value = value.ToString();
+        }
+
+        public Node(string value)
+        {
+            Left = null;
+            Right = null;
+            Value = value;
+        }
     }
 }
